35fa3b02bd03e0c6d1338425457df95ef52c5991
brandmorg <morgan.fnf@gmail.com>
2023-06-02 00:18:41 -0400

    finalizing menu and menu item tests

--- menuApp/app/models/menu_item.rb
+++ menuApp/app/models/menu_item.rb
@@ -1,5 +1,5 @@
 class MenuItem < ApplicationRecord
-    belongs_to :menus
+    # belongs_to :menus
     validates :name, presence: true # no null items
     validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
--- menuApp/test/models/menu_item_test.rb
+++ menuApp/test/models/menu_item_test.rb
@@ -1,14 +1,30 @@
 require 'test_helper'
 
 class MenuItemTest < ActiveSupport::TestCase
-    test '1 - MenuItems Price > 0' do
-        menu = Menu.create(name: 'testMenu')
+    test '1 - Menu Item Has Name' do
+        menuItem = MenuItem.create(
+            name: 'testBurger',
+            price: 0
+        )
+        assert_equal(menuItem.name, 'testBurger')
+    end
+
+    test '2 - MenuItems Price > 0' do
         menuItem = MenuItem.create(
             name: 'testTaco',
             price: 4.50,
-            menus_id: menu.id
         )
-
-        assert.equal(menuItem.menu.id, menu.id)
+        assert(menuItem.price > 0)
     end
+
+    test '3 - MenuItems Price <= 0 Return Error' do
+        assert_raises(ActiveRecord::RecordInvalid) do
+            MenuItem.create!(
+            name: 'testTaco',
+            price: -4.50,
+        )
+    end
+    end
+
+
 end
